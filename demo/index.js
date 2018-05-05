43525deb5f860c65a457dd8147bd6cedc287f4fe
Uncle Charlie <lixiangdong@mobike.com>
2018-05-05 20:37:57 +0800

    new demo

--- demo/index.js
+++ demo/index.js
@@ -1,27 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import { AppRegistry } from 'react-native';
+import App from './App';
 
-import React, { Component } from 'react';
-
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight
-} from 'react-native';
-
-import BaiduMapDemo from './BaiduMapDemo';
-
-class rn extends Component {
-  render() {
-    return (
-      <BaiduMapDemo />
-    );
-  }
-}
-
-AppRegistry.registerComponent('rn', () => rn);
+AppRegistry.registerComponent('demo', () => App);
