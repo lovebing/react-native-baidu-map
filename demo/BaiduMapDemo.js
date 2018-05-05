32ceca7c2c32cf9fe8639826b35e649b96318cda
Uncle Charlie <lixiangdong@mobike.com>
2018-05-05 13:10:33 +0800

    use git repo lib

--- demo/BaiduMapDemo.js
+++ demo/BaiduMapDemo.js
@@ -2,16 +2,9 @@
  * @author lovebing
  */
 
-import React, {
-  Component,
-  PropTypes
-} from 'react';
+import React, { Component, PropTypes } from "react";
 
-import {
-  MapView,
-  MapTypes,
-  Geolocation
-} from 'react-native-baidu-map';
+import { MapView, MapTypes, Geolocation } from "react-native-baidu-map";
 
 import {
   Button,
@@ -20,12 +13,11 @@ import {
   Text,
   View,
   TouchableHighlight
-} from 'react-native';
+} from "react-native";
 
-import Dimensions from 'Dimensions';
+import Dimensions from "Dimensions";
 
 export default class BaiduMapDemo extends Component {
-
   constructor() {
     super();
 
@@ -38,25 +30,27 @@ export default class BaiduMapDemo extends Component {
       },
       trafficEnabled: false,
       baiduHeatMapEnabled: false,
-      markers: [{
-        longitude: 113.981718,
-        latitude: 22.542449,
-        title: "Window of the world"
-      },{
-        longitude: 113.995516,
-        latitude: 22.537642,
-        title: ""
-      }]
+      markers: [
+        {
+          longitude: 113.981718,
+          latitude: 22.542449,
+          title: "Window of the world"
+        },
+        {
+          longitude: 113.995516,
+          latitude: 22.537642,
+          title: ""
+        }
+      ]
     };
   }
 
-  componentDidMount() {
-  }
+  componentDidMount() {}
 
   render() {
     return (
       <View style={styles.container}>
-        <MapView 
+        <MapView
           trafficEnabled={this.state.trafficEnabled}
           baiduHeatMapEnabled={this.state.baiduHeatMapEnabled}
           zoom={this.state.zoom}
@@ -65,79 +59,99 @@ export default class BaiduMapDemo extends Component {
           marker={this.state.marker}
           markers={this.state.markers}
           style={styles.map}
-          onMarkerClick={(e) => {
+          onMarkerClick={e => {
             console.warn(JSON.stringify(e));
           }}
-          onMapClick={(e) => {
-          }}
-        >
-        </MapView>
+          onMapClick={e => {}}
+        />
 
         <View style={styles.row}>
-          <Button title="Normal" onPress={() => {
-            this.setState({
-              mapType: MapTypes.NORMAL
-            });
-          }} />
-          <Button style={styles.btn} title="Satellite" onPress={() => {
-            this.setState({
-              mapType: MapTypes.SATELLITE
-            });
-          }} />
-
-          <Button style={styles.btn} title="Locate" onPress={() => {
-            console.warn('center', this.state.center);
-            Geolocation.getCurrentPosition()
-              .then(data => {
-                console.warn(JSON.stringify(data));
-                this.setState({
-                  zoom: 15,
-                  marker: {
-                    latitude: data.latitude,
-                    longitude: data.longitude,
-                    title: 'Your location'
-                  },
-                  center: {
-                    latitude: data.latitude,
-                    longitude: data.longitude,
-                    rand: Math.random()
-                  }
-                });
-              })
-              .catch(e =>{
-                console.warn(e, 'error');
-              })
-          }} />
-        </View>
-
-        <View style={styles.row}>
-          <Button title="Zoom+" onPress={() => {
-            this.setState({
-              zoom: this.state.zoom + 1
-            });
-          }} />
-          <Button title="Zoom-" onPress={() => {
-            if(this.state.zoom > 0) {
+          <Button
+            title="Normal"
+            onPress={() => {
               this.setState({
-                zoom: this.state.zoom - 1
+                mapType: MapTypes.NORMAL
               });
-            }
-            
-          }} />
+            }}
+          />
+          <Button
+            style={styles.btn}
+            title="Satellite"
+            onPress={() => {
+              this.setState({
+                mapType: MapTypes.SATELLITE
+              });
+            }}
+          />
+
+          <Button
+            style={styles.btn}
+            title="Locate"
+            onPress={() => {
+              console.warn("center", this.state.center);
+              Geolocation.getCurrentPosition()
+                .then(data => {
+                  console.warn(JSON.stringify(data));
+                  this.setState({
+                    zoom: 15,
+                    marker: {
+                      latitude: data.latitude,
+                      longitude: data.longitude,
+                      title: "Your location"
+                    },
+                    center: {
+                      latitude: data.latitude,
+                      longitude: data.longitude,
+                      rand: Math.random()
+                    }
+                  });
+                })
+                .catch(e => {
+                  console.warn(e, "error");
+                });
+            }}
+          />
         </View>
 
         <View style={styles.row}>
-          <Button title="Traffic" onPress={() => {
-            this.setState({
-              trafficEnabled: !this.state.trafficEnabled
-            });
-          }} />
+          <Button
+            title="Zoom+"
+            onPress={() => {
+              this.setState({
+                zoom: this.state.zoom + 1
+              });
+            }}
+          />
+          <Button
+            title="Zoom-"
+            onPress={() => {
+              if (this.state.zoom > 0) {
+                this.setState({
+                  zoom: this.state.zoom - 1
+                });
+              }
+            }}
+          />
+        </View>
 
-          <Button title="Baidu HeatMap" onPress={() => {
-            this.setState({
-              baiduHeatMapEnabled: !this.state.baiduHeatMapEnabled
-            });
-          }} />
+        <View style={styles.row}>
+          <Button
+            title="Traffic"
+            onPress={() => {
+              this.setState({
+                trafficEnabled: !this.state.trafficEnabled
+              });
+            }}
+          />
+
+          <Button
+            title="Baidu HeatMap"
+            onPress={() => {
+              this.setState({
+                baiduHeatMapEnabled: !this.state.baiduHeatMapEnabled
+              });
+            }}
+          />
         </View>
       </View>
     );
@@ -146,18 +160,18 @@ export default class BaiduMapDemo extends Component {
 
 const styles = StyleSheet.create({
   row: {
-    flexDirection: 'row',
+    flexDirection: "row",
     height: 40
   },
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height - 200,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height - 200,
     marginBottom: 16
   }
 });
